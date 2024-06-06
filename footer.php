8cc36166894d960c584e8631da77872d7da109cb
Rushita Bhawsar <rushitabhawsar@gmail.com>
2024-06-06 18:56:34 +0530

    6-06-2024 / 6:56pm

--- footer.php
+++ footer.php
@@ -1,63 +1,59 @@
 </main>
+<!------------------ FOOTER SECTION -------------------->
 <footer>
-    <div class="footer">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-2 col-md-4 col-6 text-md-start text-center">
-                    <img src="assets/img/footer-logo.png" alt="company-name" class="img-fluid">
+    <div class="container">
+        <div class="row g-lg-5 g-3">
+            <div class="col-lg-6 col-md-6 col-12">
+                <div class="footer-left">
+                    <p class="roboto-regular">We provide heat treatment facility for wood as per ISPM-15 standard to make wood free from
+                        all living insects includiing eggs and larvae.</p>
+                    <ul class="d-flex list-unstyled ms-5">
+                        <li><a href="#"><img src="assets/img/facebook.webp" alt="rps facebook"></a>
+                        </li>
+                        <li><a href="#"><img src="assets/img/twitter.webp" alt="rps twitter"></a>
+                        </li>
+                        <li><a href="#"><img src="assets/img/linkedin.webp" alt="rps linkedin"></a>
+                        </li>
+                        <li><a href="#"><img src="assets/img/pinterest.webp" alt="rps pinterest"></a>
+                        </li>
+                    </ul>
                 </div>
-                <div class="col-lg-2 col-md-4 col-6 text-center">
-                    <div class="footer-content">
-                        <img src="assets/img/footer-location.png" alt="location">
-                        <p>Hougang Central Blk 804 <br> Hougang New Town, S530804</p>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12">
+                <div class="footer-center">
+                    <div class="title">
+                        <h3 class="roboto-regular">Quick Links</h3>
                     </div>
+                    <ul class="list-unstyled">
+                        <li><a href="babool-wood-boxes.html">BABOOL WOOD BOXES</a></li>
+                        <li><a href="pine-wood.html">PINE WOOD </a></li>
+                        <li><a href="babool-wood.html">BABOOL WOOD </a></li>
+                        <li><a href="pine-wood-pallets.html">PINE WOOD PALLETS </a></li>
+                        <li><a href="pine-wood-box.html">PINE WOOD BOX</a></li>
+                        <li><a href="babool-pallets.html">BABOOL PALLETS </a></li>
+                    </ul>
                 </div>
-                <div class="col-lg-2 col-md-4 col-6 text-center">
-                    <div class="footer-content">
-                        <img src="assets/img/footer-mail.png" alt="location">
-                        <p>ricco.curtain@yahoo.com.sg</p>
+            </div>
+            <div class="col-lg-3 col-md-12 col-12">
+                <div class="footer-right">
+                    <div class="title">
+                        <h3 class="roboto-regular">Contact</h3>
                     </div>
-                </div>
-                <div class="col-lg-2 col-md-4 col-6 text-center">
-                    <div class="footer-content">
-                        <img src="assets/img/footer-telephone.png" alt="location">
-                        <p>+65 6286 0776</p>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-4 col-6 text-center">
-                    <div class="footer-content">
-                        <img src="assets/img/footer-phone.png" alt="location">
-                        <p>+65 9668 3817</p>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-4 col-6 text-center">
-                    <div class="footer-content">
-                        <img src="assets/img/footer-fb.png" alt="location">
-                        <p>Facebook: Ricco Curtain</p>
+                    <div class="location">
+                        <p class="roboto-regular">39 Shree Ram Nagar Colony, Dhar Naka Mhow - M.P. 453441</p>
+                        <p class="roboto-regular"> <a href="tel:+919926993051">Phone: +919926993051</a></p>
+                        <p class="roboto-regular"> <a href="mailto:rspmhow@gmail.com">Email:rspmhow@gmail.com</a></p>
                     </div>
                 </div>
             </div>
-            <hr class="hr-line">
-            <div class="footer-test py-md-2">
-                <div class="row">
-                    <div class="col-lg-9 col-md-7 col-12">
-                        <p class="test-start m-0">Copyright by Ricco Curtain Design Pte Ltd @ 2024 All rights reserved.</p>
-                    </div>
-                    <div class="col-lg-3 col-md-5 col-12 text-md-end">
-                        <a href="https://websentials.com.sg/" target="_blank" class="text-decoration-none">Web Design By Websentials <img src="assets/img/websentials-logo.webp" alt="" class="ms-1" width="20"></a>
-                    </div>
+            <div class="col-12">
+                <div class="footer-copy-right">
+                    <p class="roboto-regular">Copyright © 2022. All rights reserved. Design &amp; Developed by <span><a href="https://www.starwebindia.com/" target="_blank">Star Web India</a></span></p>
                 </div>
             </div>
         </div>
     </div>
 </footer>
-<div class="fixed-icon">
-    <div class="whatsapp">
-        <a href="#" target="_blank">
-            <img src="assets/img/whatsapp.png" width="100" alt="whatsapp icon">
-        </a>
-    </div>
-</div>
 
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.bundle.min.js"></script>
--- header.php
+++ header.php
@@ -39,46 +39,72 @@
 </head>
 
 <body>
+    <!------------------ HEADER SECTION -------------------->
     <header>
-        <div class="top-navbar">
-            <nav class="navbar navbar-expand-lg">
-                <div class="container">
-                    <a class="navbar-brand" href="#"><img src="assets/img/logo-real.webp" alt="logo"></a>
-                    <div class="header-right d-lg-flex d-none">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-text" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-text" href="#">Home</a>
-                            </li>
-                        </ul>
+        <div class="top-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="d-flex justify-content-between">
+                            <div class="header-right d-none d-lg-flex py-2">
+                                <ul class="list-unstyled roboto-regular d-flex">
+                                    <li class="roboto-medium fs-6"><i class="fa fa-user-o" aria-hidden="true"></i> Call us</li>
+                                    <li> <a href="tel:+91-9926993051">+91-9926993051</a></li>
+                                </ul>
+                                <ul class="list-unstyled roboto-regular d-flex">
+                                    <li class="roboto-medium fs-6"><i class="fa fa-clock-o" aria-hidden="true"></i> Working Hours</li>
+                                    <li> <a href="#">Mon - Sat 10 to 5pm</a></li>
+                                </ul>
+                                <ul class="list-unstyled roboto-regular d-flex">
+                                    <li class="roboto-medium fs-6"><i class="fa fa-envelope-o" aria-hidden="true"></i> Email Us</li>
+                                    <li><a href="mailto:rspmhow@gmail.com">rspmhow@gmail.com</a></li>
+                                </ul>
+                            </div>
+                            <div class="right-btn d-none d-lg-flex justify-content-between">
+                               <a href="contact.html" class="btn btn-primary"><i class="fa fa-sign-in me-2"></i> Request a Quote</a>
+                            </div>
+                        </div>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-header">
+            <nav class="navbar navbar-expand-lg sticky p-0">
+                <div class="container">
+                <a class="navbar-brand" href="index.php"><img src="assets/img/logo-real.webp" alt="logo"></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 roboto-regular">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                <a class="nav-link" href="#">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
+                                <a class="nav-link" href="about.html">About</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
+                                    Our-Products
                                 </a>
                                 <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li>
-                                        <hr class="dropdown-divider">
+                                    <li><a class="dropdown-item" href="babool-wood-boxes.html">BABOOL WOOD BOXES</a>
                                     </li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    <li><a class="dropdown-item" href="pine-wood.html">PINE WOOD </a></li>
+                                    <li><a class="dropdown-item" href="babool-wood.html">BABOOL WOOD </a></li>
+                                    <li><a class="dropdown-item" href="pine-wood-pallets.html">PINE WOOD PALLETS </a>
+                                    </li>
+                                    <li><a class="dropdown-item" href="pine-wood-box.html">PINE WOOD BOX</a></li>
+                                    <li><a class="dropdown-item" href="babool-pallets.html">BABOOL PALLETS </a></li>
                                 </ul>
                             </li>
-                        </ul> -->
+                            <li class="nav-item">
+                                <a class="nav-link" href="our-client.html">Our Clients</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact.html">Contact us</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </nav>
